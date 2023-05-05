07b3e6aabffb97d2232851b97ea8e6943bfd8018
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2023-05-05 16:49:59 +0300

    refactor: my solution first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,13 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const text = path.resolve('./01-read-file/text.txt');
+const stream = new fs.ReadStream(text, {encoding: 'utf-8'});
 
-try {
-  const data = fs.readFileSync(
-    text,
-    'utf8'
-  );
-  console.log(data);
-} catch (err) {
-  console.error(err);
-}
+stream.on('data', (text) => console.log(text));
