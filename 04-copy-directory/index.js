e19015b54a2bcb673564ad739e12678af54bba24
PavelKonstantinovich <pashakonstantinovich@gmail.com>
2023-05-07 16:26:30 +0300

    feat: add my solution fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyFolder = path.join(__dirname,'files');
+const filesFolder =path.join(__dirname,'files-copy');
+
+const deleteFolder = ()=> fs.rm(filesFolder,{force: true,recursive: true},() =>cloning());
+deleteFolder();
+async function cloning(){
+  await  fs.promises.readdir(copyFolder).then (files =>{
+    fs.mkdir(filesFolder,{recursive: true},() => console.log('Директория создана!'));
+    files.forEach(file => {
+      fs.promises.copyFile(path.join(copyFolder,file),path.join(filesFolder,file));
+    });
+  });
+}
